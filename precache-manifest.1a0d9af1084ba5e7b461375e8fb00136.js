9edb466751d800ce4d8fc7b488adbb2c4222fb3e
mredmo5 <38364533+mredmo5@users.noreply.github.com>
2020-07-09 22:46:19 -0400

    Updates

---
+++ precache-manifest.1a0d9af1084ba5e7b461375e8fb00136.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8631c89010f151ee230ae69f9cf430ba",
+    "url": "/startheband/index.html"
+  },
+  {
+    "revision": "8af1be4f73a33ac31967",
+    "url": "/startheband/static/css/main.556ef96d.chunk.css"
+  },
+  {
+    "revision": "6d517a7bf635b569b6ed",
+    "url": "/startheband/static/js/2.df5f7a24.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/startheband/static/js/2.df5f7a24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af1be4f73a33ac31967",
+    "url": "/startheband/static/js/main.628186df.chunk.js"
+  },
+  {
+    "revision": "6aae35d2bf9842bfdb3e",
+    "url": "/startheband/static/js/runtime-main.4f3569d2.js"
+  },
+  {
+    "revision": "7067d1a03847cc08168c63a2d952c002",
+    "url": "/startheband/static/media/Logo.7067d1a0.png"
+  },
+  {
+    "revision": "82dfc8de6a69caa253ff94c4b9feda01",
+    "url": "/startheband/static/media/star1.82dfc8de.jpg"
+  },
+  {
+    "revision": "68ff4fa5083f0b8ef2bcbd9bfd16dbab",
+    "url": "/startheband/static/media/star5.68ff4fa5.jpg"
+  },
+  {
+    "revision": "557be95989927292e8571cb4cb086aa7",
+    "url": "/startheband/static/media/star6.557be959.jpg"
+  }
+]);
